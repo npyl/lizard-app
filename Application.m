af291f6a321de2c9e95999c97a826d2b5e1c458f
sonotone13 <sonotone13@3265611c-c17b-11de-93d8-795327aa6b9b>
2009-10-31 14:23:07 +0000

    first shot with pciroot detection / smbios cells synchronization

--- Application.m
+++ Application.m
@@ -17,10 +17,10 @@
 	// remove DS_Store
 	NSError *error;
 	NSFileManager *theManager = [NSFileManager defaultManager];
-	NSString *showExtraPath = [[NSUserDefaults standardUserDefaults] stringForKey:@"Extra Folder"];
-	[theManager removeItemAtPath:[showExtraPath stringByAppendingPathComponent:@"Themes/.DS_Store"] error:&error];
+	[theManager removeItemAtPath:@"/Extra/Themes/.DS_Store" error:&error];
 	
 	//paramètre dossier Extra par defaut
+	NSString *showExtraPath = [[NSUserDefaults standardUserDefaults] stringForKey:@"Extra Folder"];
 	if (!showExtraPath) {
 		[[NSUserDefaults standardUserDefaults] setValue:@"/Extra" forKey:@"Extra Folder"];
 	}
